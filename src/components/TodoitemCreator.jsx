fb2f53fc16bbbc712bfc5f9532e30c8b71eef221
WY <ablack1029@gmail.com>
2022-09-12 15:52:53 +0900

    useSetRecoilState

---
+++ src/components/TodoitemCreator.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { useSetRecoilState } from 'recoil';
+import { todoListState } from '../recoil';
+
+export const TodoitemCreator = () => {
+  const [inputValue, setInputValue] = useState('');
+  const setTodoList = useSetRecoilState(todoListState);
+
+  const handleInputChange = ({ target: { value } }) => {
+    setInputValue(value);
+  };
+
+  const addItem = () => {
+    setTodoList((oldList) => [
+      ...oldList,
+      {
+        id: getId(),
+        text: inputValue,
+        inComplete: false,
+      },
+    ]);
+  };
+
+  let id = 0;
+  const getId = () => {
+    return id++;
+  };
+
+  return (
+    <div>
+      <input type='text' value={inputValue} onChange={handleInputChange} />
+      <button onClick={addItem}>Add</button>
+    </div>
+  );
+};
